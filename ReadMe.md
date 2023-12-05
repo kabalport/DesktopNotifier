2db64965a236898675336a1b2bd02ebcf17fc5f5
최대열 <dychoi@brainednet.com>
2023-12-05 10:14:01 +0900

    3

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,4 @@
 pip install -r requirements.txt
+
+
+pyinstaller --onefile --windowed main.py
--- notifier.py
+++
@@ -1,6 +0,0 @@
-from win10toast import ToastNotifier
-
-toaster = ToastNotifier()
-
-def send_notification(title, message):
-    toaster.show_toast(title, message, duration=10)
